c585a9e0ff59bbffa234398a968adc226cd67383
RiyaJadhav1803 <166241201+RiyaJadhav1803@users.noreply.github.com>
2024-07-17 12:25:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const mongoose=require("mongoose");
 const app=express();
 const cookieParser=require("cookie-parser");
-const PORT=5000;
+const PORT=process.env.PORT || 5000;
 require('dotenv').config();
 const stripe=require("stripe")(process.env.STRIPE_SECRET_KEY);
 const cors=require("cors");
